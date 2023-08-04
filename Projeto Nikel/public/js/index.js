e42d111310201ccf1a97a9c5e59d20a0ade6d05a
Lariproggamer <141454316+Lariproggamer@users.noreply.github.com>
2023-08-04 17:40:36 -0300

    Add files via upload

---
+++ Projeto Nikel/public/js/index.js
@@ -0,0 +1,98 @@
+const myModal = new bootstrap.Modal("#register-modal");
+let logged = sessionStorage.getItem("logged");
+const session = localStorage.getItem("session");
+
+checkLogged();
+
+
+//login
+document.getElementById("login-form").addEventListener("submit", function(e) {
+    e.preventDefault();
+
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    const session = document.getElementById("session").checked;
+
+    const account = getAccount(email);
+
+    if (!account) {
+        alert("Algo deu errado. Verifique o usuário ou a senha.");
+        return;
+    }
+
+    if (account) {
+        if (account.password !== password) {
+            alert("Algo deu errado. Verifique o usuário ou a senha.");
+            return;
+        }
+
+        saveSession(email, session)
+
+        window.location.href = "home.html";
+    }
+});
+
+
+//criar conta
+document.getElementById("create-form").addEventListener("submit", function(e) {
+    e.preventDefault();
+
+    const email = document.getElementById("email-create-input").value;
+    const password = document.getElementById("password-create-input").value;
+
+    if (email.length < 5) {
+        alert("Insira um e-mail válido.");
+        return;
+    }
+
+    if (password.length < 4) {
+        alert("Preencha a senha com no mínimo 4 dígitos.");
+        return;
+    }
+
+    saveAccount({
+        login: email,
+        password: password,
+        transactions: []
+    });
+
+    myModal.hide();
+
+    alert(" Usuário criado com sucesso.");
+});
+
+//checar login
+function checkLogged() {
+    if (session) {
+        sessionStorage.setItem("logged", session);
+        logged = session;
+    }
+
+    if (logged) {
+        saveSession(logged, session);
+
+        window.location.href = "home.html";
+    }
+}
+
+function saveAccount(data) {
+    localStorage.setItem(data.login, JSON.stringify(data));
+}
+
+function saveSession(data, saveSession) {
+    if (saveSession) {
+        localStorage.setItem("session", data);
+    }
+
+    sessionStorage.setItem("logged", data);
+}
+
+function getAccount(key) {
+    const account = localStorage.getItem(key);
+
+    if (account) {
+        return JSON.parse(account);
+    }
+
+    return "";
+}
